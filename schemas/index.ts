61f6bd5b9496ea6caee5a088c08cac3fa1b60185
cholkany <xxx>
2023-10-25 23:26:22 +0200

    Feat: New schema in index.ts

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,5 +4,6 @@ import post from './post'
 import author from './author'
 import project from './project'
 import news from './news'
+import review from './testimonial'
 
-export const schemaTypes = [post, author, category, blockContent,project,news]
+export const schemaTypes = [post, author, category, blockContent,project,news,review]
